4bb5a730ece8b7e3d5012508983dc8a84d8b6e1d
sigor1202 <igor.1202@hotmail.com>
2025-05-23 20:00:18 -0300

    importação explicita do crypto

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'crypto';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -7,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*', // ou restrinja se preferir
+    origin: '*', 
   });
   
 
